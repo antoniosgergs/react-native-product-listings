ea7934d7a052849fb4b9f6544d3b279962b7042a
Antonios Gerges <antonios.gerges4@gmail.com>
2025-06-04 21:29:46 +0300

    feat: add shopping card

--- src/store/shoppingCart.js
+++ src/store/shoppingCart.js
@@ -4,30 +4,30 @@ const useShoppingCart = create((set, get) => ({
   shoppingCart: [],
   addItemToShoppingCart: ({ item }) => {
     const cart = get().shoppingCart;
-    const itemFound = cart.find(val => val._id === item._id);
+    const itemFound = cart.find(val => val._id === item?._id);
 
     // If item found --> increase the count else add it with count 1
     const newShoppingCart = itemFound
-      ? cart.map(val =>  val._id === item._id ? {...val, count: val.count + 1} : val)
+      ? cart.map(val =>  val._id === item?._id ? {...val, count: val.count + 1} : val)
       : [...cart, {...item, count:1}];
 
     set({shoppingCart: newShoppingCart});
   },
   removeItemFromShoppingCart: ({ item }) => {
     const cart = get().shoppingCart;
-    const itemFound = cart.find(val => val._id === item._id);
+    const itemFound = cart.find(val => val._id === item?._id);
 
     // If item found --> decrease the count if > 1 else remove it from shoppingCart
     const newShoppingCart = itemFound?.count > 1
-      ? cart.map(val => val._id === item._id ? { ...val, count: val.count - 1 } : val)
-      : cart.filter(val => val._id !== item._id);
+      ? cart.map(val => val._id === item?._id ? { ...val, count: val.count - 1 } : val)
+      : cart.filter(val => val._id !== item?._id);
 
     set({ shoppingCart: newShoppingCart });
   },
   deleteItemFromShoppingCart: ({item})=>{
     const cart = get().shoppingCart;
 
-    const newShoppingCart = cart.filter(val => val._id !== item._id);
+    const newShoppingCart = cart.filter(val => val._id !== item?._id);
 
     set({ shoppingCart: newShoppingCart });
   },
