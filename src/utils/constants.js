19c3ce840918e061aeddad130ef296db009463ee
Antonios Gerges <antonios.gerges4@gmail.com>
2025-06-04 21:05:04 +0300

    feat: add environment variable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,9 @@
+import Config from "react-native-config";
+
+const ENV =  Config.ENV;
+const API_URL =  Config.API_URL;
+
 const APP_PREFIX = 'antonios_product_listing://';
 const APP_PREFIXES = [APP_PREFIX];
 
-export {APP_PREFIX, APP_PREFIXES};
+export {APP_PREFIX, APP_PREFIXES, API_URL, ENV};
